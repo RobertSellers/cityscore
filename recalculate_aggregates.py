7e7d58c3836efcb629efc25595e3a9ac51eb4553
RobertSellers <robert.sellers@azimuth1.com>
2019-08-28 16:35:10 -0400

    aggregate dimension level functional

--- recalculate_aggregates.py
+++ recalculate_aggregates.py
@@ -49,8 +49,9 @@ try:
     df_agg["multiplier"] = df_agg["importance"].astype(float)/100
     df_agg['prptn_indiv'] = df_agg['multiplier']/df_agg['multiplier'].sum()
     df_agg['prprtn_indicator'] = df_agg.groupby(['dimension','indicator'])['prptn_indiv'].transform(lambda x: x.sum())
-    df_agg['prprtn_dimension'] = df_agg['prprtn_indicator'].groupby(df_agg['dimension']).transform(lambda x:  x/x.sum())
-
+    df_agg['prprtn_dimension'] = df_agg['importance'].astype(float).groupby(df_agg['dimension']).transform(lambda x:  x/x.sum())
+    df_agg['prprtn_dimension'] = (df_agg.groupby(['dimension', 'indicator'])['prprtn_dimension'].transform('sum'))
+    df_agg_2 =df_agg.groupby(['dimension','indicator'])['prptn_indiv'].agg('sum').reset_index()
     # organize
     del df_agg['dimension_b']
     df_agg = df_agg[['name','dimension', 'indicator','importance','prptn_indiv','prprtn_indicator','prprtn_dimension','count','multiplier']]
@@ -60,37 +61,51 @@ try:
     fields_found = df_agg['name'].unique().tolist()
     fields_found.insert(0, 'city')
     raw_df = arcgis_table_to_dataframe(scores_table, fields_found)
+    agg_calc_cols = ['city', 'dimension', 'indicator', 'cum_sum', 'multiplier']
+    lvl2List = []
     # loop through city predictor calculations / Score Tree
     for index, row in df_agg.iterrows():
         # loop / use cursor on existing aggregate feature class
         with arcpy.da.UpdateCursor(agg_feature,['city',str(row['indicator']),str(row['dimension']), 'composite']) as cursor:
+            # scores = []
             for r in cursor:
+
                 # update each indicator field (level 3)
                 agg_indicator = df_agg.loc[df_agg['name'] == str(row['name']), 'indicator'].iloc[0]
-                cum_sum = 0
+                cum_sum = 0.0
                 for index, r2 in df_agg.loc[df_agg['indicator'] == agg_indicator].iterrows():
                     raw_data_score = raw_df.loc[raw_df['city'] == r[0], r2['name']].iloc[0]
+                    #scores.append(raw_data_score)
                     cum_sum += (raw_data_score * r2['prptn_indiv']) / r2['prprtn_indicator']
                 r[1] = cum_sum
-                # update each dimension field (level 2)
+
+                # load each dimension field (level 2)
                 agg_dimension = df_agg.loc[df_agg['name'] == str(row['name']), 'dimension'].iloc[0]
-                cum_sum = 0
-                for index, r2 in df_agg.loc[df_agg['dimension'] == agg_dimension].iterrows():
-                    raw_data_score = raw_df.loc[raw_df['city'] == r[0], r2['name']].iloc[0]
-                    cum_sum += (raw_data_score * r2['prprtn_dimension'])
-                r[2] = cum_sum
-                # update composite field (level 1)
-                composite = 0
-                for index, r3 in df_agg.iterrows():
-                    raw_data_score = raw_df.loc[raw_df['city'] == r[0], r2['name']].iloc[0]
-                    composite += (raw_data_score * r2['prptn_indiv'])
-                r[3] = composite
+
+                for index, r2 in df_agg_2.iterrows():
+
+                    if r2['indicator'] == str(row['indicator']) and r2['dimension'] == str(row['dimension']):
+                        lvl2List.append([r[0], row['dimension'],row['indicator'],float(cum_sum),r2['prptn_indiv']])
+
+                cursor.updateRow(r)
+    agg_calc_cols = pd.DataFrame(lvl2List, columns=agg_calc_cols)
+    agg_calc_cols = agg_calc_cols.drop_duplicates(['city', 'dimension', 'indicator'])
+    agg_calc_cols= agg_calc_cols.assign(col = agg_calc_cols.cum_sum * agg_calc_cols.multiplier).groupby(['city','dimension'],as_index=False).col.sum()
+
+    for index, row in df_agg.iterrows():
+        # loop / use cursor on existing aggregate feature class
+        with arcpy.da.UpdateCursor(agg_feature,['city',str(row['dimension']), 'composite']) as cursor:
+            # scores = []
+            for r in cursor:
+                #agg_dimension = df_agg.loc[df_agg['name'] == str(row['name']), 'dimension'].iloc[0]
+                r[1] = agg_calc_cols.loc[(agg_calc_cols['city'] == r[0]) & (str(row['dimension']) == agg_calc_cols['dimension']), 'col'].iloc[0]
+                r[2] = 200
                 cursor.updateRow(r)
 
     # export data to desktop for Calculation QA
-    export_original = raw_df.to_csv(r'C:/Users/rober/Desktop/df_tot.csv', index = None, header=True)
-    export_csv = df_agg.to_csv (r'C:/Users/rober/Desktop/df_agg.csv', index = None, header=True)
-
+    #export_original = raw_df.to_csv(r'C:/Users/rober/Desktop/df_tot.csv', index = None, header=True)
+    #export_csv = df_agg.to_csv (r'C:/Users/rober/Desktop/df_agg.csv', index = None, header=True)
+    #export_csv2 = agg_calc_cols.to_csv (r'C:/Users/rober/Desktop/agg_calc_cols.csv', index = None, header=True)
 except Exception, ErrorDesc:
     sErr = "ERROR:\n" + str(ErrorDesc)
     arcpy.AddError(sErr)
