b0d24ba0346a16dc6b18294bbdf6ab61dad77fa6
RobertSellers <robert.sellers@azimuth1.com>
2019-08-29 11:50:16 -0400

    comments and schema fix

--- initialize_tables.py
+++ initialize_tables.py
@@ -1,86 +1,167 @@
 import arcpy, sys, string, os
 
 try:
+
     arcpy.env.workspace = "in_memory"
     scores_fgdb = arcpy.GetParameterAsText(0)
     arcpy.env.workspace = scores_fgdb
-    
+    sr = arcpy.SpatialReference(4326)
+
     if arcpy.Exists("scores_table"):
         fc_cities = scores_fgdb+"/scores_table"
+
     else:
 
         city_score_fields = (
-            #("city", "TEXT", None),("state", "TEXT", None),
-            ("gmp1yr", "DOUBLE"),("gmp3yr", "DOUBLE"),("lightEmit", "DOUBLE"),("deposit5yr", "DOUBLE"),
-            ("majLocInd", "DOUBLE"),("majNatInd", "DOUBLE"),("instOutpt", "DOUBLE"),
-            ("retailSales", "DOUBLE"),("totalPop", "DOUBLE"),("tradeArSls", "DOUBLE"),
-            ("tradeArPop", "DOUBLE"),("unemplRate", "DOUBLE"),("civWrkforc", "DOUBLE"),
-            ("medHhInc", "DOUBLE"),("pCapEdu", "DOUBLE"),("goBndRate", "DOUBLE"),
-            ("hmless5yr", "DOUBLE"),("hmless10k", "DOUBLE"),("cannib", "DOUBLE"),
-            ("mayorTenr", "DOUBLE"),("mayorVtShr", "DOUBLE"),("riskIdxPrs", "DOUBLE"),
-            ("whatworks", "DOUBLE"),("openData", "DOUBLE"),("greenIdx", "DOUBLE"),
-            ("pCapHsptl", "DOUBLE"),("pCapParks", "DOUBLE"),("pCapSewer", "DOUBLE"),
-            ("pctCapEx", "DOUBLE"),("startup", "DOUBLE"),("hiGrowth", "DOUBLE"),
-            ("bigTech", "DOUBLE"),("popDens", "DOUBLE"),("coreRvrsn", "DOUBLE"),
-            ("fiber", "DOUBLE"),("rsrchUni", "DOUBLE"),("rsrchHsptl", "DOUBLE"),
-            ("convSpace", "DOUBLE"),("proSports", "DOUBLE"),("patentGrw", "DOUBLE"),
-            ("avgTemp", "DOUBLE"),("dvstyIdx", "DOUBLE"),("costLive", "DOUBLE"),
-            ("prvSchCost", "DOUBLE"),("prchPwr", "DOUBLE"),("uniRdpCap", "DOUBLE"),
-            ("uniRd5yr", "DOUBLE"),("kwlgWrkrs", "DOUBLE"),("pCapUtil", "DOUBLE"),
-            ("jobGwth3yr", "DOUBLE"),("popGwth8yr", "DOUBLE"),("millnPct", "DOUBLE"),
-            ("milln5yr", "DOUBLE"),("bachelors", "DOUBLE"),("steamJobs", "DOUBLE"),
-            ("steamCmmt", "DOUBLE"),("steamEarn", "DOUBLE"),("steamComp", "DOUBLE"),
-            ("sixFigHH", "DOUBLE"),("incInqlty", "DOUBLE"),("houseAppr", "DOUBLE"),
-            ("aggInc", "DOUBLE"),("strtDens", "DOUBLE"),("pctDev", "DOUBLE"),
-            ("cityAge", "DOUBLE"),("histCore", "DOUBLE"),("meanSlope", "DOUBLE"),
-            ("bdrockDpth", "DOUBLE"),("natHaz", "DOUBLE"),("clmteRsli", "DOUBLE"),
-            ("nimbyAve", "DOUBLE"),("cstrCstIdx", "DOUBLE"),("cstrUnion", "DOUBLE"),
-            ("supplyPipe", "DOUBLE"),("mfStockPct", "DOUBLE"),("prcElasSup", "DOUBLE"),
-            ("prcElasDmd", "DOUBLE"),("taxBurden", "DOUBLE"),("taxRate", "DOUBLE"),
-            ("pCapPrpTax", "DOUBLE"),("mtg2Rent", "DOUBLE"),("rent2Inc", "DOUBLE"),
-            ("aptCPPI", "DOUBLE"),("cppiTrgh", "DOUBLE"),("cprateTrgh", "DOUBLE"),
-            ("revPaf3yr", "DOUBLE"),("absrbPace", "DOUBLE"),("trnscVol", "DOUBLE"),
-            ("rentOcc", "DOUBLE"),("reMktAve", "DOUBLE"),("reMktSent", "DOUBLE"),
-            ("pCapTollRv", "DOUBLE"),("pCapHwy", "DOUBLE"),("airptPGrw", "DOUBLE"),
-            ("airptTrvTm", "DOUBLE"),("mobMode", "DOUBLE"),("railSrv", "DOUBLE"),
-            ("pMileTrans", "DOUBLE"),("transStops", "DOUBLE"),("trvTmInnr", "DOUBLE"),
-            ("interstates", "DOUBLE"),("congCost", "DOUBLE"),("urbanVMT", "DOUBLE"),
-            ("avgCommute", "DOUBLE"),("pubTrnsCom", "DOUBLE")) 
-        sr = arcpy.SpatialReference(4326)
+            ('gmp1yr', 'FLOAT'),
+            ('gmp3yr', 'FLOAT'),
+            ('lightEmit', 'FLOAT'),
+            ('deposit5yr', 'FLOAT'),
+            ('majLocInd', 'SHORT'),
+            ('majNatInd', 'SHORT'),
+            ('instOutpt', 'FLOAT'),
+            ('retailSales', 'FLOAT'),
+            ('totalPop', 'LONG'),
+            ('tradeArSls', 'LONG'),
+            ('tradeArPop', 'LONG'),
+            ('unemplRate', 'FLOAT'),
+            ('civWrkforc', 'LONG'),
+            ('medHhInc', 'LONG'),
+            ('pCapEdu', 'FLOAT'),
+            ('goBndRate', 'SHORT'),
+            ('hmless5yr', 'FLOAT'),
+            ('hmless10k', 'LONG'),
+            ('cannib', 'FLOAT'),
+            ('mayorTenr', 'FLOAT'),
+            ('mayorVtShr', 'FLOAT'),
+            ('riskIdxPrs', 'SHORT'),
+            ('whatworks', 'SHORT'),
+            ('openData', 'FLOAT'),
+            ('greenIdx', 'FLOAT'),
+            ('pCapHsptl', 'FLOAT'),
+            ('pCapParks', 'FLOAT'),
+            ('pCapSewer', 'FLOAT'),
+            ('pctCapEx', 'FLOAT'),
+            ('startup', 'FLOAT'),
+            ('hiGrowth', 'FLOAT'),
+            ('bigTech', 'SHORT'),
+            ('popDens', 'FLOAT'),
+            ('coreRvrsn', 'FLOAT'),
+            ('fiber', 'FLOAT'),
+            ('rsrchUni', 'FLOAT'),
+            ('rsrchHsptl', 'LONG'),
+            ('convSpace', 'LONG'),
+            ('proSports', 'SHORT'),
+            ('patentGrw', 'FLOAT'),
+            ('avgTemp', 'FLOAT'),
+            ('dvstyIdx', 'FLOAT'),
+            ('costLive', 'FLOAT'),
+            ('prvSchCost', 'LONG'),
+            ('prchPwr', 'FLOAT'),
+            ('uniRdpCap', 'FLOAT'),
+            ('uniRd5yr', 'FLOAT'),
+            ('kwlgWrkrs', 'FLOAT'),
+            ('pCapUtil', 'FLOAT'),
+            ('jobGwth3yr', 'FLOAT'),
+            ('popGwth8yr', 'FLOAT'),
+            ('millnPct', 'FLOAT'),
+            ('milln5yr', 'FLOAT'),
+            ('bachelors', 'FLOAT'),
+            ('steamJobs', 'FLOAT'),
+            ('steamCmmt', 'LONG'),
+            ('steamEarn', 'FLOAT'),
+            ('steamComp', 'LONG'),
+            ('sixFigHH', 'FLOAT'),
+            ('incInqlty', 'FLOAT'),
+            ('houseAppr', 'FLOAT'),
+            ('aggInc', 'LONG'),
+            ('strtDens', 'FLOAT'),
+            ('pctDev', 'FLOAT'),
+            ('cityAge', 'LONG'),
+            ('histCore', 'FLOAT'),
+            ('meanSlope', 'FLOAT'),
+            ('bdrockDpth', 'FLOAT'),
+            ('natHaz', 'SHORT'),
+            ('clmteRsli', 'FLOAT'),
+            ('nimbyAve', 'LONG'),
+            ('cstrCstIdx', 'FLOAT'),
+            ('cstrUnion', 'FLOAT'),
+            ('supplyPipe', 'FLOAT'),
+            ('mfStockPct', 'FLOAT'),
+            ('prcElasSup', 'FLOAT'),
+            ('prcElasDmd', 'FLOAT'),
+            ('taxBurden', 'FLOAT'),
+            ('taxRate', 'FLOAT'),
+            ('pCapPrpTax', 'FLOAT'),
+            ('mtg2Rent', 'FLOAT'),
+            ('rent2Inc', 'FLOAT'),
+            ('aptCPPI', 'FLOAT'),
+            ('cppiTrgh', 'FLOAT'),
+            ('cprateTrgh', 'FLOAT'),
+            ('revPaf3yr', 'FLOAT'),
+            ('absrbPace', 'LONG'),
+            ('trnscVol', 'FLOAT'),
+            ('rentOcc', 'FLOAT'),
+            ('reMktAve', 'FLOAT'),
+            ('reMktSent', 'FLOAT'),
+            ('pCapTollRv', 'FLOAT'),
+            ('pCapHwy', 'FLOAT'),
+            ('airptQual', 'SHORT'),
+            ('airptPGrw', 'FLOAT'),
+            ('airptTrvTm', 'FLOAT'),
+            ('mobMode', 'SHORT'),
+            ('railSrv', 'FLOAT'),
+            ('pMileTrans', 'LONG'),
+            ('transStops', 'SHORT'),
+            ('trvTmInnr', 'SHORT'),
+            ('interstates', 'SHORT'),
+            ('congCost', 'LONG'),
+            ('urbanVMT', 'FLOAT'),
+            ('avgCommute', 'FLOAT'),
+            ('pubTrnsCom', 'FLOAT'),
+            )
+
         fc_cities = arcpy.CreateFeatureclass_management(scores_fgdb, "scores_table", "POINT", spatial_reference=sr)  
         arcpy.CopyFeatures_management("data_xy",fc_cities)
         for fc_field in city_score_fields:  
             arcpy.AddField_management(fc_cities, *fc_field)
+
     if arcpy.Exists("scores_aggregate"):
         fc_score_agg = scores_fgdb+"/scores_aggregate"
+
     else:
-        sr = arcpy.SpatialReference(4326)
+
         #fc_score_agg = arcpy.CreateTable_management(scores_fgdb, "scores_aggregate")
         fc_score_agg = arcpy.CreateFeatureclass_management(scores_fgdb, "scores_aggregate", "POINT", spatial_reference=sr)  
         arcpy.CopyFeatures_management("data_xy",fc_score_agg)
-        agg_fields = (("composite", "DOUBLE"),("economy", "DOUBLE"),
-        ("govern", "DOUBLE"),("growth", "DOUBLE"),
-        ("develop", "DOUBLE"),("market", "DOUBLE"),
-        ("transport", "DOUBLE"),("govern", "DOUBLE"),
-        ("economy", "DOUBLE"),("industry", "DOUBLE"),
-        ("jobs", "DOUBLE"),("output", "DOUBLE"),
-        ("pop", "DOUBLE"),("trd_area", "DOUBLE"),
-        ("spending", "DOUBLE"),("cap_prdct", "DOUBLE"),
-        ("leadershp", "DOUBLE"),("progress", "DOUBLE"),
-        ("density", "DOUBLE"),("commerce", "DOUBLE"),
-        ("instit", "DOUBLE"),("resources", "DOUBLE"),
-        ("talent", "DOUBLE"),("steam", "DOUBLE"),
-        ("construc", "DOUBLE"),("enviro", "DOUBLE"),
-        ("phys", "DOUBLE"),("supply", "DOUBLE"),
-        ("momentum", "DOUBLE"),("afforda", "DOUBLE"),
-        ("multifam", "DOUBLE"),("pricing", "DOUBLE"),
-        ("air", "DOUBLE"),("rail", "DOUBLE"),("invest", "DOUBLE"),("wealth", "DOUBLE"),
-        ("road", "DOUBLE"),("usblty", "DOUBLE"),("historic", "DOUBLE"),("homeless", "DOUBLE"),("intellect", "DOUBLE")
+        agg_fields = (
+        ("composite", "FLOAT"),("economy", "FLOAT"),
+        ("govern", "FLOAT"),("growth", "FLOAT"),
+        ("develop", "FLOAT"),("market", "FLOAT"),
+        ("transport", "FLOAT"),("govern", "FLOAT"),
+        ("economy", "FLOAT"),("industry", "FLOAT"),
+        ("jobs", "FLOAT"),("output", "FLOAT"),
+        ("pop", "FLOAT"),("trd_area", "FLOAT"),
+        ("spending", "FLOAT"),("cap_prdct", "FLOAT"),
+        ("leadershp", "FLOAT"),("progress", "FLOAT"),
+        ("density", "FLOAT"),("commerce", "FLOAT"),
+        ("instit", "FLOAT"),("resources", "FLOAT"),
+        ("talent", "FLOAT"),("steam", "FLOAT"),
+        ("construc", "FLOAT"),("enviro", "FLOAT"),
+        ("phys", "FLOAT"),("supply", "FLOAT"),
+        ("momentum", "FLOAT"),("afforda", "FLOAT"),
+        ("multifam", "FLOAT"),("pricing", "FLOAT"),
+        ("air", "FLOAT"),("rail", "FLOAT"),
+        ("invest", "FLOAT"),("wealth", "FLOAT"),
+        ("road", "FLOAT"),("usblty", "FLOAT"),("historic", "FLOAT"),
+        ("homeless", "FLOAT"),("intellect", "FLOAT")
         )
         for fc_field in agg_fields:  
             arcpy.AddField_management(fc_score_agg, *fc_field)
+
     arcpy.SetParameter(1, fc_cities)
     arcpy.SetParameter(2, fc_score_agg)
+	
 except Exception, ErrorDesc:
     sErr = "ERROR:\n" + str(ErrorDesc)
     arcpy.AddError(sErr)
--- recalculate_aggregates.py
+++ recalculate_aggregates.py
@@ -1,6 +1,7 @@
 import arcpy, sys, string, os
 import pandas as pd
     
+# utility function to go from arcgis feature class attribute table to pandas dataframe
 def arcgis_table_to_dataframe(in_fc, input_fields, query="", skip_nulls=False, null_values=None):
     """Function will convert an arcgis table into a pandas dataframe with an object ID index, and the selected
     input fields. Uses TableToNumPyArray to get initial data."""
@@ -16,6 +17,8 @@ def arcgis_table_to_dataframe(in_fc, input_fields, query="", skip_nulls=False, n
 
 try:
     arcpy.env.workspace = "in_memory"
+
+    # Access modelbuilder parameters
     score_tree = arcpy.GetParameterAsText(0) #metadata table 
     scores_table = arcpy.GetParameterAsText(1) #the master data table to be checked
     agg_feature = arcpy.GetParameterAsText(2) #the agg  data table to be updated
@@ -34,16 +37,16 @@ try:
         else:
             # field has values - to be aggregated
             fields_wdata.append(field)
+
     fields_wdata.remove('city')
     fields_wdata.remove('state')
-    df = pd.DataFrame(columns=["firstname", "lastname"])
 
     # select from score_tree where field is found & calculate intermediaries
     tree_query = "name IN (" + "' - '".replace('-', ",".join('"{}"'.format(i) for i in fields_wdata)) + ")"
     tree_query = tree_query.replace('"', '').replace("' ", "'").replace(" '", "'").replace(",","','")
     fields_subset = ['name', 'dimension', 'indicator', 'importance']
+    
     df_agg = arcgis_table_to_dataframe(score_tree, fields_subset, query = tree_query)
-
     df_agg_counts = df_agg.groupby(["dimension", "indicator"]).dimension.agg('count').to_frame('count').reset_index()
     df_agg = df_agg.set_index('indicator').join(df_agg_counts.set_index('indicator'), rsuffix='_b').reset_index()
     df_agg["multiplier"] = df_agg["importance"].astype(float)/100
@@ -52,6 +55,7 @@ try:
     df_agg['prprtn_dimension'] = df_agg['importance'].astype(float).groupby(df_agg['dimension']).transform(lambda x:  x/x.sum())
     df_agg['prprtn_dimension'] = (df_agg.groupby(['dimension', 'indicator'])['prprtn_dimension'].transform('sum'))
     df_agg_2 =df_agg.groupby(['dimension','indicator'])['prptn_indiv'].agg('sum').reset_index()
+
     # organize
     del df_agg['dimension_b']
     df_agg = df_agg[['name','dimension', 'indicator','importance','prptn_indiv','prprtn_indicator','prprtn_dimension','count','multiplier']]
@@ -79,33 +83,33 @@ try:
                     cum_sum += (raw_data_score * r2['prptn_indiv']) / r2['prprtn_indicator']
                 r[1] = cum_sum
 
-                # load each dimension field (level 2)
+                # this section pre-populates pre-calculated aggregates to feature for dimension. current loop 
+                # seems incompatible with adding via cursor
                 agg_dimension = df_agg.loc[df_agg['name'] == str(row['name']), 'dimension'].iloc[0]
 
                 for index, r2 in df_agg_2.iterrows():
-
                     if r2['indicator'] == str(row['indicator']) and r2['dimension'] == str(row['dimension']):
-                        lvl2List.append([r[0], row['dimension'],row['indicator'],float(cum_sum),r2['prptn_indiv']])
+                        lvl2List.append([r[0], row['dimension'], row['indicator'], float(cum_sum), r2['prptn_indiv']])
 
                 cursor.updateRow(r)
+
     agg_calc_cols = pd.DataFrame(lvl2List, columns=agg_calc_cols)
     agg_calc_cols = agg_calc_cols.drop_duplicates(['city', 'dimension', 'indicator'])
     agg_calc_cols= agg_calc_cols.assign(col = agg_calc_cols.cum_sum * agg_calc_cols.multiplier).groupby(['city','dimension'],as_index=False).col.sum()
 
+    # this section adds pre-calculated aggregates to feature for dimension
     for index, row in df_agg.iterrows():
         # loop / use cursor on existing aggregate feature class
         with arcpy.da.UpdateCursor(agg_feature,['city',str(row['dimension']), 'composite']) as cursor:
-            # scores = []
             for r in cursor:
-                #agg_dimension = df_agg.loc[df_agg['name'] == str(row['name']), 'dimension'].iloc[0]
-                r[1] = agg_calc_cols.loc[(agg_calc_cols['city'] == r[0]) & (str(row['dimension']) == agg_calc_cols['dimension']), 'col'].iloc[0]
-                r[2] = 200
+                r[1] = 999#agg_calc_cols.loc[(agg_calc_cols['city'] == r[0]) & (str(row['dimension']) == agg_calc_cols['dimension']), 'col'].iloc[0]
+                r[2] = 999
                 cursor.updateRow(r)
 
     # export data to desktop for Calculation QA
     #export_original = raw_df.to_csv(r'C:/Users/rober/Desktop/df_tot.csv', index = None, header=True)
-    #export_csv = df_agg.to_csv (r'C:/Users/rober/Desktop/df_agg.csv', index = None, header=True)
-    #export_csv2 = agg_calc_cols.to_csv (r'C:/Users/rober/Desktop/agg_calc_cols.csv', index = None, header=True)
+    export_csv = df_agg.to_csv (r'C:/Users/rober/Desktop/df_agg.csv', index = None, header=True)
+    export_csv2 = agg_calc_cols.to_csv (r'C:/Users/rober/Desktop/agg_calc_cols.csv', index = None, header=True)
 except Exception, ErrorDesc:
     sErr = "ERROR:\n" + str(ErrorDesc)
     arcpy.AddError(sErr)
